5fd2fe3fa97d02963979e95e867ef05840acb2c8
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-07-22 14:34:16 -0400

    header and nav bar added as components

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import { useQuery } from "@apollo/client";
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">How It Works</a>
+        </li>
+        <li>
+          <a href="#">Vote Here</a>
+        </li>
+        <li>
+          <a href="#">Your Submission</a>
+        </li>
+        <li>
+          <a href="#">Virtual Library</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
