820c439fcb894195a5af0e46a1a390fce1d7dd5b
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-07-22 12:29:18 -0400

    committing to pull, but also added info to app.js and index.js to allow for hello world deplpoyment

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+// reportWebVitals();
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,12 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
 
-
+const Home = () => {
+  return (
+    <main>
+      <div>Hello World</div>
+    </main>
+  );
+};
 
 export default Home;
