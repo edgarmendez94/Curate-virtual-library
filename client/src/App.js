08b8350c3a7aaefe9135a33accdd3b9402b8cd50
Taeyong Lee <d104601@hotmail.com>
2022-07-25 00:10:43 -0400

    Merged Navbar and Header to Header. Removed some duplicated html codes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Signup from "./pages/Sign-up";
-
-
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -18,38 +15,15 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <header id="header">
-      <h1>curate</h1>
-      <h2>A Digital Virtual Museum</h2>
-      <button id="signInBtn" type="button" class="btn btn-primary">
-        Sign In
-      </button>
-      <button id="signUpBtn" type="button" class="btn btn-primary">
-        Sign Up
-      </button>
-    </header>
-    <nav>
-      <ul>
-        <li><a href="#">How It Works</a></li>
-        <li><a href="#">Vote Here</a></li>
-        <li><a href="#">Your Submission</a></li>
-        <li><a href="#">Virtual Library</a></li>
-      </ul>
-    </nav>
+    <ApolloProvider client={client}> 
       <Router>
-      <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
-      </Routes>
-        </div>
-        <div>
           <Header />
-        </div>
         <div>
-          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<Signup />} />
+          </Routes>
         </div>
         <div>
           <Footer />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,19 +2,33 @@ import React from "react";
 // import { useQuery } from "@apollo/client";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-const Header = () => {
-  return (<>
-
+const Nav = () => {
+  return (
     <header id="header">
-      <h1>curate</h1>
-      <h2>A Digital Virtual Museum</h2>
+      <nav>
+        <h1>curate</h1>
+        <h2>A Digital Virtual Museum</h2>
 
-     <Link to={"/login"}> <button id="signInBtn" type="button" className="btn btn-primary" > Sign In </button> </Link> 
-    <Link to={"/sign-up"}> <button id="signUpBtn" type="button" className="btn btn-primary">Sign Up</button> </Link> 
+        <Link to={"/login"}> <button id="signInBtn" type="button" className="btn btn-primary" > Sign In </button> </Link> 
+        <Link to={"/sign-up"}> <button id="signUpBtn" type="button" className="btn btn-primary">Sign Up</button> </Link> 
+        
+        <ul>
+          <li>
+            <a href="#">How It Works</a>
+          </li>
+          <li>
+            <a href="#">Vote Here</a>
+          </li>
+          <li>
+            <a href="#">Your Submission</a>
+          </li>
+          <li>
+            <a href="#">Virtual Library</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-
-    </>
   );
 };
 
-export default Header;
+export default Nav;
