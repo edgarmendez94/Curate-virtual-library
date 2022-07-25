c3570722985a6d64f0ccec5d0aa544010a26b24a
Tazaev <tonymurtazaev@gmail.com>
2022-07-25 10:22:38 -0400

    added typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,32 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type User {
+    id: Int!
+    firstName: String
+    lastName: String
+    email: String
+  }
 
+  type Images {
+    id: Int!
+    title: String
+    description: String
+    onDisplay: Boolean
+    posterId: [user]!
+    votes: Int
+  }
+
+  type Votes {
+    id: Int!
+    images: [Images]!
+    authorId: ID!
+    votes: Int
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
 `;
 
 module.exports = typeDefs;
