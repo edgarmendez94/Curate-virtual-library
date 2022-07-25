dbd5180738fd111219dcba1de71acdbdd253b656
elliotzacharywaxman <elliot@climatedao.xyz>
2022-07-25 12:06:56 -0400

    fixing merge conflict due to node modules and resolvers issue

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Images} = require('../models');
+const { User, Images } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const { AuthenticationError } = require('apollo-server-express');
@@ -57,42 +57,3 @@ const resolvers = {
         },
     },
 };
-
-const resolvers = {
-    Query: {
-      users: async () => {
-        return User.find();
-      },
-      },
-
-    //   assuming we need addImage & addVote mutations ? 
-      Mutation: {
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw new AuthenticationError('No user found with this email address');
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw new AuthenticationError('Incorrect credentials');
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-        },
-    
-
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-          }
-
-        }}
-        
-        
-        module.exports = resolvers;
