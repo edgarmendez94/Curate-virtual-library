cfc5ce6d361198797dc6d12c535c88378e60d884
elliotzacharywaxman <elliot@climatedao.xyz>
2022-07-25 09:57:34 -0400

    merging

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,6 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
-<<<<<<< HEAD
-
-// import schema from Images.js
 const imagesSchema = require('./Images');
-
-=======
-const imagesSchema = require('./Images');
->>>>>>> dd1a27ed2e291629246f0b79713c7f8d9e072def
 const userSchema = new Schema(
   {
     username: {
@@ -35,10 +28,6 @@ const userSchema = new Schema(
     },
   }
 );
-<<<<<<< HEAD
-
-=======
->>>>>>> dd1a27ed2e291629246f0b79713c7f8d9e072def
 // hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
@@ -47,30 +36,13 @@ userSchema.pre('save', async function (next) {
   }
   next();
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> dd1a27ed2e291629246f0b79713c7f8d9e072def
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-<<<<<<< HEAD
-
-=======
->>>>>>> dd1a27ed2e291629246f0b79713c7f8d9e072def
 // when we query a user, we'll also get another field called `imageCount` with the number of stored images we have
 userSchema.virtual('imageCount').get(function () {
   return this.storedimages.length;
 });
-<<<<<<< HEAD
-
-
-const User = model('User', userSchema);
-
-module.exports = User;
-
-=======
 const User = model('User', userSchema);
 module.exports = User;
->>>>>>> dd1a27ed2e291629246f0b79713c7f8d9e072def
