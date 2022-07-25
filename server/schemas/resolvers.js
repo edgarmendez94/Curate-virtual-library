22b046fb5e8301ff8f401fe361d5a99aa0ddc60f
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-25 01:53:48 -0400

    added login mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,7 +10,24 @@ const resolvers = {
       },
 
 
-
+      Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
       
-module.exports = resolvers;
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+    }
 }
+}
+module.exports = resolvers;
