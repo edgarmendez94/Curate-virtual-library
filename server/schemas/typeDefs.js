709b3cdc5b73a9027df04e0a2449a9c1800c5347
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-25 12:32:23 -0400

    forced commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,37 +1,23 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-
-  type User {
-    id: Int!
-    firstName: String
-    lastName: String
+type User {
+    _id: ID
+    username: String
     email: String
-  }
+    password: String
+}
 
-  type Images {
-    id: Int!
-    title: String
-    description: String
-    onDisplay: Boolean
-    posterId: [user]!
-    votes: Int
-  }
-
-  type Votes {
-    id: Int!
-    images: [Images]!
-    authorId: ID!
-    votes: Int
-  }
-  type Auth {
-    token: ID
+type Auth {
+    token: ID!
     user: User
+  }
+
+
   
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
   }
 `;
 
