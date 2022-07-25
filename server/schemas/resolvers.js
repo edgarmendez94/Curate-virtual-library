00aea255f85f09022fc4582210eec2c0c4ab8d58
Edgar Mendez <edgar.ym26@gmail.com>
2022-07-25 01:51:40 -0400

    resolvers started , user querey

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,16 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Images} = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+      users: async () => {
+        return User.find();
+      },
+      },
 
 
+
+      
 module.exports = resolvers;
+}
