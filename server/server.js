51377af2c089a087a477398117f7872cd38a4da6
d104601 <d104601@hotmail.com>
2022-07-26 15:55:54 -0400

    fixed account page bug

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const fileUpload = require('express-fileupload');
 
-
+const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -12,6 +12,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 app.use(express.urlencoded({ extended: false }));
