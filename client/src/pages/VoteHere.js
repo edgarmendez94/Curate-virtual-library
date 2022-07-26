82000f5b99c788909b87c450d6df04e9a71a3903
elliotzacharywaxman <elliot@climatedao.xyz>
2022-07-26 18:13:12 -0400

    test files added

--- client/src/pages/VoteHere.js
+++ client/src/pages/VoteHere.js
@@ -1,11 +1,40 @@
-import React from 'react'
-// import keyArr from '../../../server/server';
+// import React from 'react'
+// // import keyArr from '../../../server/server';
 
-export default function VoteHere() {
+// export default function VoteHere() {
+//     return (
+//         <div>
+//             <div>VoteHere</div>
+//             <img src="/images/a0d690bb8950c339ea968bccf01b69ac"></img>
+//         </div>
+//     )
+// }
+
+import React from 'react';
+import ImageList from '../components/ImagesList';
+import { useQuery } from '@apollo/client';
+import { QUERY_IMAGES } from '../utils/queries';
+
+const VoteHere = () => {
+    const { data } = useQuery(QUERY_IMAGES);
+    const images = data?.images || [];
+
     return (
-        <div>
-            <div>VoteHere</div>
-            <img src="/images/a0d690bb8950c339ea968bccf01b69ac"></img>
-        </div>
-    )
-}
+        <main>
+            <div className="flex-row justify-center">
+                <div className="col-12 col-md-10 my-3">
+                    {loading ? (
+                        <div>Loading...</div>
+                    ) : (
+                        <ImageList
+                            images={images}
+                            title="Here's the current images..."
+                        />
+                    )}
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default VoteHere;
